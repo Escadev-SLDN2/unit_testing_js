1bd470dd0e4f8a005b79911744a790e594fbafb9
Nicolas Vauché <hello@nicolasvauche.net>
2020-07-20 12:02:25 +0200

    fix

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const convertEuros = () => {
+  const numberToConvert = document.getElementById('numberToConvert')
+  const numberConverted = document.getElementById('numberConverted')
+  numberConverted.value = euroToDollar(numberToConvert.value)
+}
