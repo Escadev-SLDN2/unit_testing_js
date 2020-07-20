e399c33b2281ad008fc8f62021a764ba27bfc754
Nicolas Vauché <hello@nicolasvauche.net>
2020-07-20 12:30:19 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const convertEuros = () => {
   const numberToConvert = document.getElementById('numberToConvert')
-  const numberConverted = document.getElementById('numberConverted')
-  numberConverted.value = euroToDollar(numberToConvert.value)
+
+  const numberInUSD = document.getElementById('numberInUSD')
+  const numberInGBP = document.getElementById('numberInGBP')
+  const numberInJPY = document.getElementById('numberInJPY')
+
+  numberInUSD.value = convertEuro(numberToConvert.value, 'USD')
+  numberInGBP.value = convertEuro(numberToConvert.value, 'GBP')
+  numberInJPY.value = convertEuro(numberToConvert.value, 'JPY')
 }
